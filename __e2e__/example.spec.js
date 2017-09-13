42886c33f20312eb34c18b619264125b5e298e9f
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-09-14 00:13:36 +0700

    Fix test for todo app

--- __e2e__/example.spec.js
+++ __e2e__/example.spec.js
@@ -1,9 +1,33 @@
-describe('Example', () => {
-  it('should have homepage screen', async () => {
-    await expect(element(by.id('homepageTitle'))).toBeVisible();
+describe('Todo App', () => {
+  it('should have header view', async () => {
+    await expect(element(by.id('headerView'))).toBeVisible();
   });
-  it('should have button', async () => {
-    await element(by.id('juangFuck')).tap();
-    await expect(element(by.id('showJuang'))).toBeVisible();
+
+  it('should have todos view', async () => {
+    await expect(element(by.id('todosView'))).toBeVisible();
+  });
+
+  it('should have app title', async () => {
+    await expect(element(by.id('appTitle'))).toBeVisible();
+  });
+
+  it('should have a text input', async () => {
+    await expect(element(by.id('mainInput'))).toBeVisible();
+  });
+
+  it('should have a submit button', async () => {
+    await expect(element(by.id('submitButton'))).toBeVisible();
+  });
+
+  it('should have submit a new todo', async () => {
+    await element(by.id('mainInput')).typeText('Buy a food');
+    await element(by.id('submitButton')).tap();
+    await expect(element(by.id('deleteButton0'))).toBeVisible();
+    await expect(element(by.id('todo0'))).toHaveText('Buy a food');
+  });
+
+  it('should have remove a todo', async () => {
+    await element(by.id('deleteButton0')).tap();
+    await expect(element(by.id('todo0'))).toNotExist();
   });
 });
