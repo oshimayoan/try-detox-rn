64562c595801b6970d1d8e064f78072add104cd7
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-09-13 23:11:10 +0700

    Add dummy detox test

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default class App extends React.Component {
+  state;
+
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showJuang: false,
+    };
+  }
+
   render() {
     return (
       <View style={styles.container} testID="homepageTitle">
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          title="Juang Gob"
+          onPress={() => this.setState({showJuang: true})}
+          testID="juangFuck"
+        />
+        {this.state.showJuang ? (
+          <Text testID="showJuang">Show Juang</Text>
+        ) : null}
       </View>
     );
   }
--- __e2e__/example.spec.js
+++ __e2e__/example.spec.js
@@ -2,4 +2,8 @@ describe('Example', () => {
   it('should have homepage screen', async () => {
     await expect(element(by.id('homepageTitle'))).toBeVisible();
   });
+  it('should have button', async () => {
+    await element(by.id('juangFuck')).tap();
+    await expect(element(by.id('showJuang'))).toBeVisible();
+  });
 });
