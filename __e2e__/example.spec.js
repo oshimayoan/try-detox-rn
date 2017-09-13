146790a1dda10cca27e3e9b058d060b4b36dad5b
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-09-13 18:18:25 +0700

    Change jest setup for detox

--- __e2e__/example.spec.js
+++ __e2e__/example.spec.js
@@ -1,10 +1,4 @@
-import {reloadApp} from 'detox-expo-helpers';
-
 describe('Example', () => {
-  beforeEach(async () => {
-    await reloadApp();
-  });
-
   it('should have homepage screen', async () => {
     await expect(element(by.id('homepageTitle'))).toBeVisible();
   });
